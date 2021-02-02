df0a58246d67f832de6827f2c20d7349e97d9e9a
jaedi <jerrydieugenio@gmail.com>
2021-02-02 22:52:45 +0800

    Removed user registration restrictions (Temporary)

--- routes/auth.php
+++ routes/auth.php
@@ -11,11 +11,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/register', [RegisteredUserController::class, 'create'])
-                ->middleware('auth') //change to auth
+                ->middleware('guest') //change to auth
                 ->name('register');
 
 Route::post('/register', [RegisteredUserController::class, 'store'])
-                ->middleware('auth'); //change to auth
+                ->middleware('guest'); //change to auth
 
 Route::get('/login', [AuthenticatedSessionController::class, 'create'])
                 ->middleware('guest')
